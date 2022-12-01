45a76c02683609e208a0917f62b5f19a300ff483
Fixxoo <rajpalmal40@gmail.com>
2022-12-01 23:09:17 +0530

    Initial Learnings

---
+++ helloscript.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/bash
+
+# echo "Hello Bash yo" {Like Sout in java}
+
+# cat > filename.txt # {^d command-d is used to stop the terminal}  {it is a command that is used to output a data on another file using the terminal}
+#  cat >> filename.txt {^d command-d is used to stop the terminal}   {it is a command that is used to append the data with the previous data}
+
+# cat << kreative         {this is a type of output that is shown in the terminal itself}       
+# hee hee kya kartis hmmmm
+# kreative
+
+#  echo $1 $2 $3 # {It will take input from the terminal and the $1 and all are the representative of how many words can it take and show }
+# args = ("$@") {this $@ can take unlimited numbers from the user through the terminal}
+# echo $@ {this will print out unlimited }
+# echo $# {this will give out the length of the array that we have given}
+
